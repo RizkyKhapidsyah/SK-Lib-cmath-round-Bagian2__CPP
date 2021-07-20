9cc414d615c098ee0528325b029393b805913afe
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-20 09:06:11 +0700

    Add project files.

---
+++ SK-Lib-cmath-round-Bagian2__CPP/Source.cpp
@@ -0,0 +1,33 @@
+
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	double x = 12.5, y = 13.3, z = 14.8;
+
+	cout << "Nearest value of x :" << round(x) << "\n";
+	cout << "Nearest value of y :" << round(y) << "\n";
+	cout << "Nearest value of z :" << round(z) << "\n";
+
+	cout << "lround(-0.0) = " << lround(-0.0) << "\n";
+	cout << "lround(2.3) = " << lround(2.3) << "\n";
+	cout << "lround(2.5) = " << lround(2.5) << "\n";
+	cout << "lround(2.7) = " << lround(2.7) << "\n";
+	cout << "lround(-2.3) = " << lround(-2.3) << "\n";
+	cout << "lround(-2.5) = " << lround(-2.5) << "\n";
+	cout << "lround(-2.7) = " << lround(-2.7) << "\n";
+
+	cout << "llround(-0.01234) = " << llround(-0.01234) << "\n";
+	cout << "llround(2.3563) = " << llround(2.3563) << "\n";
+	cout << "llround(2.555) = " << llround(2.555) << "\n";
+	cout << "llround(2.7896) = " << llround(2.7896) << "\n";
+	cout << "llround(-2.323) = " << llround(-2.323) << "\n";
+	cout << "llround(-2.5258) = " << llround(-2.5258) << "\n";
+	cout << "llround(-2.71236) = " << llround(-2.71236) << "\n";
+
+	_getch();
+	return 0;
+}
